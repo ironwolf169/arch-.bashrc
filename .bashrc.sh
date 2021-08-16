75341805046999753a29ccd36d3bfd15957a0a13
ironwolf269 <88604362+ironwolf169@users.noreply.github.com>
2021-08-16 11:20:10 -0700

    Update .bashrc.sh

--- .bashrc.sh
+++ .bashrc.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+#This will place the aliases I use into .bashrc
+
+echo"
+
+alias update='sudo pacman -Syu'
+
+alias remove='sudo pacman -Rs'
+
+alias get='git clone'
+
+alias delete='rm -R'
+
+alias halt='shutdown now'
+
+alias system='systemctl'" | tee -a ~/.bashrc
